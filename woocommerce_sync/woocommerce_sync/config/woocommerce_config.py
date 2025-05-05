d7d783a04e3a98ade630266f9bd3631733fc4ada
nahomaraya <nahomaraya8@gmail.com>
2025-05-05 19:26:51 +0300

    added url

--- woocommerce_sync/woocommerce_sync/config/woocommerce_config.py
+++ woocommerce_sync/woocommerce_sync/config/woocommerce_config.py
@@ -5,7 +5,7 @@ This file contains the WooCommerce API configuration settings.
 
 # WooCommerce API Configuration
 WOOCOMMERCE_CONFIG = {
-    "url": "",  # Your WooCommerce store URL (e.g., "https://your-store.com")
+    "url": "http://order.eco2.ca",  # Your WooCommerce store URL (e.g., "https://your-store.com")
     "consumer_key": "ck_20147c5f5be907ce1aaf5c5da7e3e3e455416687",  # Your WooCommerce consumer key
     "consumer_secret": "cs_6cadf54c66d8efba4731c4c750e68febbebad9a1",  # Your WooCommerce consumer secret
     "version": "wc/v3",  # WooCommerce API version
