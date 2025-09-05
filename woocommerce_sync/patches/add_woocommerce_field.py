2d0c38104ee337481623406ac1c218ba822f80a7
nahomaraya <nahomaraya8@gmail.com>
2025-09-05 16:03:31 -0700

    modifed patch

--- woocommerce_sync/patches/add_woocommerce_field.py
+++ woocommerce_sync/patches/add_woocommerce_field.py
@@ -2,15 +2,33 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
-    custom_fields = {
-        "Sales Order": [
-            dict(
-                fieldname="woocommerce_order_id",
-                fieldtype="Data",
-                label="Woocommerce Order Id",
-                insert_after="owner",
-                read_only=0
-            )
-        ]
-    }
-    create_custom_fields(custom_fields, update=True)
+    fields_to_add = [
+        {
+            "dt": "Sales Order",
+            "fieldname": "woocommerce_order_id",
+            "fieldtype": "Data",
+            "label": "Woocommerce Order Id",
+            "insert_after": "title",
+        },
+        {
+            "dt": "Customer",
+            "fieldname": "woocommerce_customer_id",
+            "fieldtype": "Data",
+            "label": "Woocommerce Customer Id",
+            "insert_after": "title",
+        },
+        {
+            "dt": "Item",
+            "fieldname": "woocommerce_product_id",
+            "fieldtype": "Data",
+            "label": "Woocommerce Product Id",
+            "insert_after": "title",
+        }
+    ]
+
+    for field in fields_to_add:
+        if not frappe.db.exists("Custom Field", {"dt": field["dt"], "fieldname": field["fieldname"]}):
+            frappe.get_doc({
+                "doctype": "Custom Field",
+                **field
+            }).insert()
